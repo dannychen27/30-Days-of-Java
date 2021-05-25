4503dd8d1f9cdc69edc3c63150d0f18a90016c36
DannyChen <dannychen27@hotmail.com>
2021-05-25 15:14:18 -0400

    I created a basic representation of various Maze objects.
    This includes:
    - MazeSolver
    - Position
    - path

---
+++ src/JavaPracticeProjects/Maze/MazeSolver.java
@@ -0,0 +1,34 @@
+package JavaPracticeProjects.Maze;
+
+import java.util.LinkedList;
+
+/**
+ * Credits to Caleb Curry for MazeSolver code.
+ *
+ * Source: https://github.com/CalebCurry/30DaysOfJava/tree/master/MazeSolver
+ */
+
+public class MazeSolver {
+
+    static int[][] maze = {
+            {2, 0, 1, 1},
+            {1, 1, 0, 1},
+            {0, 1, 1, 1}
+    };
+    // 0 = wall
+    // 1 = path
+    // 2 = destination
+
+    static LinkedList<Position> path = new LinkedList<>();
+
+
+    public static void main(String[] args) {
+        Position p = new Position(3, 0);
+        path.push(p);
+
+        int x = path.peek().x;
+        int y = path.peek().y;
+
+        maze[y][x] = 0;
+    }
+}
