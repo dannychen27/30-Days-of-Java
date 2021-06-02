574156a11c0ee1b6da5bd0d43b6b68306da46599
DannyChen <dannychen27@hotmail.com>
2021-06-02 00:16:54 -0400

    I implemented selection sort on Day 11 of 30. (June 1, 2021)
    - This includes both iterative selection sort and recursive selection sort.

---
+++ src/Algorithms/SortingAlgorithms/SelectionSort/IterativeSelectionSort.java
@@ -0,0 +1,45 @@
+package Algorithms.SortingAlgorithms.SelectionSort;
+
+import java.util.Arrays;
+
+public class IterativeSelectionSort {
+
+    public static void main(String[] args) {
+        int[] numbers = {5, 4, 3, 2, 1};
+        iterativeSelectionSort(numbers);
+        System.out.println(Arrays.toString(numbers)); // [1, 2, 3, 4, 5]
+    }
+
+    /**
+     * Sort this array.
+     */
+    private static void iterativeSelectionSort(int[] array) {
+        // loop invariant: array[0:startIndex] is sorted.
+        for (int startIndex = 0; startIndex < array.length; startIndex++) {
+            int indexOfSmallestValue = getIndexOfSmallestValue(array, startIndex);
+            swap(array, startIndex, indexOfSmallestValue);
+        }
+    }
+
+    /**
+     * Return the index of the smallest value in the subarray array[startIndex:array.length].
+     */
+    private static int getIndexOfSmallestValue(int[] array, int startIndex) {
+        int indexOfSmallestValue = startIndex;
+        for (int i = startIndex + 1; i < array.length; i++) {
+            if (array[i] < array[indexOfSmallestValue]) {
+                indexOfSmallestValue = i;
+            }
+        }
+        return indexOfSmallestValue;
+    }
+
+    /**
+     * Swap the elements at index1 and index2.
+     */
+    private static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
