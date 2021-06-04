2860d6ccf3da44dde4c5465169cf41c15308b21f
DannyChen <dannychen27@hotmail.com>
2021-06-04 18:30:36 -0400

    I added some of my progress from Day 14 of 30 of learning Java. (June 4, 2021)
    I covered:
    - the virtual keyword

---
+++ src/Constructs/Medium/Virtual/Virtual.java
@@ -0,0 +1,10 @@
+package Constructs.Medium.Virtual;
+
+public class Virtual {
+
+    public static void main(String[] args) {
+        // the virtual keyword lets subclasses override a method from its superclass.
+        // all methods are virtual by default
+        // there is no virtual keyword in java b/c it's automatic in java
+    }
+}
