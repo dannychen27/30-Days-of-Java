e26d818128fdcc4e7496a59411beeb5855c50caf
DannyChen <dannychen27@hotmail.com>
2021-06-07 15:53:22 -0400

    I added all of my progress from Day 17 of 30 of learning Java. (June 7, 2021)
    I covered:
    - lookup rules

---
+++ src/Concepts/Hard/LookupRules/Child.java
@@ -0,0 +1,17 @@
+package Concepts.Hard.LookupRules;
+
+class Child extends Parent {
+
+    int num = 10;
+    static int statNum = 20;
+
+    int getNum() {
+        System.out.print("  Child instance method: ");
+        return num;
+    }
+
+    static int getStatNum() {
+        System.out.print("  Child static method: ");
+        return statNum;
+    }
+}
