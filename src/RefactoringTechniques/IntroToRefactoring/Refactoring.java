03ad7314243377a9e824847dde01f5c7ed46a521
DannyChen <dannychen27@hotmail.com>
2021-06-08 00:41:38 -0400

    I introduced refactoring on Day 17 of 30 of learning Java. (June 7, 2021)

---
+++ src/RefactoringTechniques/IntroToRefactoring/Refactoring.java
@@ -0,0 +1,12 @@
+package RefactoringTechniques.IntroToRefactoring;
+
+public class Refactoring {
+
+    // What is refactoring?
+    // - improving the design of your code while preserving behaviour
+    // (presering behaviour is important -- otherwise you're either debugging, adding new
+    // features, removing old features, or modifying existing features)
+    // - refactoring makes other people's code, and your existing code easier to understand
+    // - refactoring lets you efficiently extend or modify your program while
+    // minimizing the risk of introducing new bugs.
+}
