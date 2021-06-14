0482fb91e56ce98d06656742a08cc56d582a3619
DannyChen <dannychen27@hotmail.com>
2021-06-14 19:04:05 -0400

    I completed the lonely integer interview problem on Day 19 of 30 of learning Java (June 14, 2021).

---
+++ src/InterviewProblems/BitManipulationProblems/LonelyInteger/LonelyInteger.java
@@ -0,0 +1,31 @@
+package InterviewProblems.BitManipulationProblems.LonelyInteger;
+
+class LonelyInteger {
+
+    public static void main(String[] args) {
+        // of course you can use a HashMap for simplicity.
+        // the best time complexity is O(n) since you have to touch every int
+        // time: O(n), space: O(n)
+
+        // however, bit manipulation allows us to save space, no need to
+        // store additional data structures.
+        int[] nums = {9, 1, 2, 3, 2, 9, 1, 7, 7};
+        System.out.println(findLonelyInteger(nums)); // 3
+
+        int[] nums2 = {1, 1, 2, 2, 3, 3, 9};
+        System.out.println(findLonelyInteger(nums2)); // 9
+    }
+
+    /**
+     * Return the odd integer out in this list.
+     *
+     * Precondition: There is only one lonely integer.
+     */
+    private static int findLonelyInteger(int[] array) {
+        int result = 0;
+        for (int value : array) {
+            result ^= value;
+        }
+        return result;
+    }
+}
