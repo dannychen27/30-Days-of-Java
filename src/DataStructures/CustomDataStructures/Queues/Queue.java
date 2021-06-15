c428c5e17429513dbc34f34dceac8beffe3fa54e
DannyChen <dannychen27@hotmail.com>
2021-06-15 19:19:38 -0400

    I implemented my own queue data structure on Day 20 of 30 of learning Java. (June 15, 2021)

---
+++ src/DataStructures/CustomDataStructures/Queues/Queue.java
@@ -0,0 +1,57 @@
+package DataStructures.CustomDataStructures.Queues;
+
+class Queue<T> {
+
+    private class Node {
+        private T value;
+        private Node next;
+
+        private Node(T value) {
+            this.value = value;
+        }
+    }
+
+    private Node head;  // remove from the head
+    private Node tail;  // add things here
+
+    public static void main(String[] args) {
+        Queue<String> queue = new Queue<>();
+        queue.enqueue("A");
+        queue.enqueue("B");
+        queue.enqueue("C");
+        System.out.println(queue.peek());  // A
+        System.out.println(queue.dequeue());  // A
+        System.out.println(queue.dequeue());  // B
+        System.out.println(queue.dequeue());  // C
+        System.out.println(queue.isEmpty());  // true
+    }
+
+    private boolean isEmpty() {
+        return head == null;
+    }
+
+    private void enqueue(T newValue) {
+        Node newNode = new Node(newValue);
+        if (tail != null) {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        if (head == null) {
+            head = newNode;
+        }
+    }
+
+    private T peek() {
+        // This may throw an exception if the queue is empty.
+        return head.value;
+    }
+
+    private T dequeue() {
+        T oldValue = head.value;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        return oldValue;
+    }
+}
