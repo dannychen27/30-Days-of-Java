6b563fc49a350dc8cb04989f95cd940ec2b9e400
DannyChen <dannychen27@hotmail.com>
2021-06-15 14:07:36 -0400

    I completed the "characters away from anagram" interview problem on Day 20 of 30 of learning Java. (June 15, 2021)

---
+++ src/InterviewProblems/HashMapProblems/CharsAwayFromAnagram/CharsAwayFromAnagram.java
@@ -0,0 +1,61 @@
+package InterviewProblems.HashMapProblems.CharsAwayFromAnagram;
+
+import java.util.*;
+
+class CharsAwayFromAnagram {
+
+    public static void main(String[] args) {
+        // Given two strings (lowercase a->z), how many characters do we need to remove from
+        // either string to make them anagrams?
+
+        // time: O(n)
+        // space: O(n) because of hashmap
+        System.out.println(charsAwayFromAnagram("hello", "billion")); // 6
+        // he from hello, and biin from billion.
+
+        System.out.println(charsAwayFromAnagram("car", "care")); // 1
+        // e from care.
+    }
+
+    private static int charsAwayFromAnagram(String word1, String word2) {
+        HashMap<Character, Integer> charCountsOfFirstWord = getCharCounts(word1);
+        HashMap<Character, Integer> charCountsOfSecondWord = getCharCounts(word2);
+        return getDelta(charCountsOfFirstWord, charCountsOfSecondWord);
+
+    }
+
+    private static HashMap<Character, Integer> getCharCounts(String word) {
+        HashMap<Character, Integer> charCounts = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            char currentCharacter = word.charAt(i);
+            if (!charCounts.containsKey(currentCharacter)) {
+                charCounts.put(currentCharacter, 1);
+            } else {
+                charCounts.put(currentCharacter, charCounts.get(currentCharacter) + 1);
+            }
+        }
+        return charCounts;
+    }
+
+
+    private static int getDelta(HashMap<Character, Integer> charCountsOfWord1,
+                                HashMap<Character, Integer> charCountsOfWord2) {
+        Set<Character> lettersInBothWords = new HashSet<>();
+        lettersInBothWords.addAll(charCountsOfWord1.keySet());
+        lettersInBothWords.addAll(charCountsOfWord2.keySet());
+
+        int delta = 0;
+        for (char character : lettersInBothWords) {
+            if (!charCountsOfWord1.containsKey(character) && charCountsOfWord2.containsKey(character)) {
+                delta += charCountsOfWord2.get(character);
+            } else if (charCountsOfWord1.containsKey(character) && !charCountsOfWord2.containsKey(character)) {
+                delta += charCountsOfWord1.get(character);
+            } else if (charCountsOfWord1.containsKey(character) && charCountsOfWord2.containsKey(character)) {
+                int difference = Math.abs(charCountsOfWord1.get(character) - charCountsOfWord2.get(character));
+                delta += difference;
+            }
+            // we exclude the case where character does not exist in either hashmap, since that would give delta = 0
+        }
+        return delta;
+    }
+}
