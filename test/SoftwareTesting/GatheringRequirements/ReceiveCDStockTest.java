63499f0c09e931fd82ce03be4f5c8e97821635a7
DannyChen <dannychen27@hotmail.com>
2021-07-04 16:29:57 -0400

    I decoupled the newStock construction from the rest of my unit test code by using factory methods inside the "ReceiveCDStockTest" test suite.

--- test/SoftwareTesting/GatheringRequirements/ReceiveCDStockTest.java
+++ test/SoftwareTesting/GatheringRequirements/ReceiveCDStockTest.java
@@ -13,13 +13,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReceiveCDStockTest {
 
+    private Library library = new Library();
+
+    private CompactDisc highwayToNowhere = new CompactDisc("Highway to Nowhere", "Drake Bell");
+    private CompactDisc soulMan = new CompactDisc("Soul Man", "Drake Bell");
+    private CompactDisc laDaDee = new CompactDisc("La Da Dee", "Cody Simpson");
+
+    private HashMap<CompactDisc, Integer> singleCD(int numCopies) {
+        HashMap<CompactDisc, Integer> cdsToStock = new HashMap<>();
+        cdsToStock.put(highwayToNowhere, numCopies);
+        return cdsToStock;
+    }
+
+    private HashMap<CompactDisc, Integer> multipleCDs() {
+        HashMap<CompactDisc, Integer> cdsToStock = new HashMap<>();
+        cdsToStock.put(highwayToNowhere, 10);
+        cdsToStock.put(soulMan, 20);
+        cdsToStock.put(laDaDee, 20);
+        return cdsToStock;
+    }
+
     @Test
     public void addSingleCopyToCDTitleAlreadyInCatalogue() throws RestockNegativeStockException {
-        Library library = new Library();
-        CompactDisc highwayToNowhere = new CompactDisc("Highway to Nowhere", "Drake Bell");
-
-        HashMap<CompactDisc, Integer> newStock = new HashMap<>();
-        newStock.put(highwayToNowhere, 10);
+        HashMap<CompactDisc, Integer> newStock = singleCD(10);
         library.receiveCDStock(newStock);
         assertEquals(10, library.getStock(highwayToNowhere));
 
@@ -30,53 +46,33 @@ public class ReceiveCDStockTest {
 
     @Test
     public void addSingleCopyToCDTitleNotAlreadyInCatalogue() throws RestockNegativeStockException {
-        Library library = new Library();
-        CompactDisc highwayToNowhere = new CompactDisc("Highway to Nowhere", "Drake Bell");
-
-        HashMap<CompactDisc, Integer> newStock = new HashMap<>();
-        newStock.put(highwayToNowhere, 10);
+        HashMap<CompactDisc, Integer> newStock = singleCD(10);
         library.receiveCDStock(newStock);
         assertEquals(10, library.getStock(highwayToNowhere));
     }
 
     @Test
     public void add0CopiesOfASingleCDToCatalogue() throws RestockNegativeStockException {
-        Library library = new Library();
-        CompactDisc highwayToNowhere = new CompactDisc("Highway to Nowhere", "Drake Bell");
-
-        HashMap<CompactDisc, Integer> newStock = new HashMap<>();
-        newStock.put(highwayToNowhere, 0);
+        HashMap<CompactDisc, Integer> newStock = singleCD(0);
         library.receiveCDStock(newStock);
         assertEquals(0, library.getStock(highwayToNowhere));
     }
 
     @Test
     public void addNegativeCopiesOfASingleCDToCatalogue() {
-        Library library = new Library();
-        CompactDisc highwayToNowhere = new CompactDisc("Highway to Nowhere", "Drake Bell");
-
-        HashMap<CompactDisc, Integer> newStock = new HashMap<>();
-        newStock.put(highwayToNowhere, -2);
+        HashMap<CompactDisc, Integer> newStock = singleCD(-2);
         assertThrows(RestockNegativeStockException.class, () -> library.receiveCDStock(newStock));
     }
 
     @Test
     public void addMultipleCopiesToCatalogue() throws RestockNegativeStockException {
-        Library library = new Library();
-        CompactDisc highwayToNowhere = new CompactDisc("Highway to Nowhere", "Drake Bell");
-        CompactDisc soulMan = new CompactDisc("Soul Man", "Drake Bell");
-        CompactDisc laDaDee = new CompactDisc("La Da Dee", "Cody Simpson");
-        HashMap<CompactDisc, Integer> newStock = new HashMap<>();
-        newStock.put(highwayToNowhere, 20);
-        newStock.put(soulMan, 10);
-        newStock.put(laDaDee, 20);
+        HashMap<CompactDisc, Integer> newStock = multipleCDs();
         library.receiveCDStock(newStock);
-        assertEquals(20, library.getStock(highwayToNowhere));
-        assertEquals(10, library.getStock(soulMan));
+        assertEquals(10, library.getStock(highwayToNowhere));
+        assertEquals(20, library.getStock(soulMan));
         assertEquals(20, library.getStock(laDaDee));
 
-        HashMap<CompactDisc, Integer> evenMoreNewStock = new HashMap<>();
-        evenMoreNewStock.put(soulMan, 10);
+        HashMap<CompactDisc, Integer> evenMoreNewStock = singleCD(10);
         library.receiveCDStock(evenMoreNewStock);
         assertEquals(20, library.getStock(highwayToNowhere));
         assertEquals(20, library.getStock(soulMan));
