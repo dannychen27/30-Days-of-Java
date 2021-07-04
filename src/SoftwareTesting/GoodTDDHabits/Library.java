5d46c75fa6371bc304985b989fa6a5b0666f11ba
DannyChen <dannychen27@hotmail.com>
2021-07-04 00:45:46 -0400

    I refactored the Library class by removing the unused getter.
    - I also replaced the declaration type of the field "catalogue" from "List" to "Collection."

--- src/SoftwareTesting/GoodTDDHabits/Library.java
+++ src/SoftwareTesting/GoodTDDHabits/Library.java
@@ -2,15 +2,10 @@ package SoftwareTesting.GoodTDDHabits;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class Library {
 
-    private List<Movie> catalogue = new ArrayList<>();
-
-    public Collection<Movie> getCatalogue() {
-        return catalogue;
-    }
+    private Collection<Movie> catalogue = new ArrayList<>();
 
     public void donate(Movie movie) {
         catalogue.add(movie);
@@ -18,6 +13,6 @@ public class Library {
     }
 
     public boolean contains(Movie movie) {
-        return getCatalogue().contains(movie);
+        return catalogue.contains(movie);
     }
 }
