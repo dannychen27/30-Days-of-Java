0c6da0091a5d757c8802399f9c55bef21038bf87
DannyChen <dannychen27@hotmail.com>
2021-07-04 00:47:22 -0400

    I decomposed this donateMovie() test case into two smaller unit tests.
    - One test case covers adding a movie to a library catalogue; the second test case covers adding a rental copy to a catalogue.

--- test/SoftwareTesting/DonateMovieTest.java
+++ test/SoftwareTesting/DonateMovieTest.java
@@ -46,32 +46,38 @@ public class DonateMovieTest {
     // TDD habit 3: write the simplest code that makes all test cases pass
 
     @Test
-    public void donateMovie() {
+    public void movieAddedToCatalogue() {
         Library library = new Library();
         Movie movie = new Movie();
         library.donate(movie);
-        assertTrue(library.contains(movie));
         // vvvv this is called a message chain
         // assertTrue(library.getCatalogue().contains(movie));
         // ^^ to resolve this message chain code smell, encapsulate it in a method.
         assertTrue(library.contains(movie));
-
-        // when we add movies to library, we add a single rental copy.
-        // we track number of available copies,
-        // then give the catalogue a default copy.
-        assertEquals(1, movie.getCopies());
-
-        // avoid overloading your tests! make your tests specific -- and singly
-        // responsible.
-
-        // now this test case has 2 reasons to fail.
-        // when a library tries to receive a donated movie, either:
-        // - the library catalogue doesn't contain the movie OR
-        // - the movie doesn't increment its copies count in the catalogue
     }
 
     // a message chain is when you navigating through a relationship to one object
     // to the next object in the chain.
     // the list part (e.g., the contains() method) is unencapsulated, so client
     // knows the getCatalogue is some kind of list/collection.
+
+    // avoid overloading your tests! make your tests specific -- and singly
+    // responsible.
+
+    // now each test case has only 1 reasons to fail.
+    // when a library tries to receive a donated movie, either:
+    // - the library catalogue doesn't contain the movie -- 1st test case fails OR
+    // - the movie doesn't increment its copies count in the catalogue -- 2nd test case fails
+
+    @Test
+    public void rentalCopyAdded() {
+        Library library = new Library();
+        Movie movie = new Movie();
+        library.donate(movie);
+
+        // when we add movies to library, we add a single rental copy.
+        // we track number of available copies,
+        // then give the catalogue a default copy.
+        assertEquals(1, movie.getCopies());
+    }
 }
