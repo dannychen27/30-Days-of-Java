c8e70afef1848eeaae47170ce83224c527b04c49
DannyChen <dannychen27@hotmail.com>
2021-07-04 02:30:29 -0400

    I added my remaining notes about TDD from the ShoppingBasket test suite to my notes about the 3 habits of TDD.

--- test/SoftwareTesting/IntroToTDDExample/ShoppingBasketTest.java
+++ test/SoftwareTesting/IntroToTDDExample/ShoppingBasketTest.java
@@ -19,17 +19,13 @@ public class ShoppingBasketTest {
         return new ShoppingBasket(shoppingBasketItems);
     }
 
-    // Determine simplest test cases...
+    // These are the simplest test cases I could come up with...
     @Test
     public void totalOfEmptyBasket() {
-        // we don't declare a ShoppingBasket, and then write tests for it
-        // we write a test that uses a ShoppingBasket, then implement it to pass the tests
-        // the IDE will tell you which classes you need to create to pass the tests
         ShoppingBasket basket = buildBasketWithItems(new ArrayList<>());
         assertEquals(0.0, basket.getTotal(), 0.0);
     }
 
-    // golden rule of TDD: we don't write source code, unless a test requires it
     @Test
     public void totalOfSingleItem() {
         ShoppingBasket basket = buildBasketWithItems(Arrays.asList(new Item(100.0, 1)));
@@ -45,7 +41,6 @@ public class ShoppingBasketTest {
         assertEquals(300.0, basket.getTotal(), 0.0);
     }
 
-    // refactoring should make subsequent test cases and source code easier to write
     @Test
     public void totalOfItemWithQuantityTwo() {
         ShoppingBasket basket = buildBasketWithItems(Arrays.asList(new Item(100.0, 2)));
