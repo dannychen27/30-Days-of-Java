99ad40943cdefd0a01b0149b9697799d844e7ecf
DannyChen <dannychen27@hotmail.com>
2021-07-04 14:59:47 -0400

    I wrote the 6th BDD test case for my "BuyCDTest" test suite that tests whether refusing to buy a single CD leaves all catalogue stock unchanged on Day 28 of 30 of learning Java. (July 4, 2021)
    - I didn't have to change any code in the Library class's buy() method.

--- test/SoftwareTesting/GatheringRequirements/BuyCDTest.java
+++ test/SoftwareTesting/GatheringRequirements/BuyCDTest.java
@@ -59,6 +59,20 @@ public class BuyCDTest {
     }
 
     @Test
+    public void buyNoCDs() throws CDNotInCatalogueException, InsufficientStockException {
+        CompactDisc highwayToNowhere = new CompactDisc("Highway to Nowhere", "Drake Bell");
+        Library library = new Library();
+
+        HashMap<CompactDisc, Integer> newStock = new HashMap<>();
+        newStock.put(highwayToNowhere, 1);
+        library.receiveCDStock(newStock);
+
+        HashMap<CompactDisc, Integer> shoppingCartItems = new HashMap<>();
+        library.buy(shoppingCartItems);
+        assertEquals(1, library.getStock(highwayToNowhere));
+    }
+
+    @Test
     public void buyMultipleCDsSuccessfully() throws CDNotInCatalogueException, InsufficientStockException {
         CompactDisc highwayToNowhere = new CompactDisc("Highway to Nowhere", "Drake Bell");
         CompactDisc soulMan = new CompactDisc("Soul Man", "Drake Bell");
