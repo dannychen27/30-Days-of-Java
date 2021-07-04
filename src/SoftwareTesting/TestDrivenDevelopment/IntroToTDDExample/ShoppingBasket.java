677ba32a9ac14b77b2c39975d7308010e8bd8f65
DannyChen <dannychen27@hotmail.com>
2021-07-04 02:00:29 -0400

    I moved my notes about feature envy into a separate text file.

--- src/SoftwareTesting/TestDrivenDevelopment/IntroToTDDExample/ShoppingBasket.java
+++ src/SoftwareTesting/TestDrivenDevelopment/IntroToTDDExample/ShoppingBasket.java
@@ -13,20 +13,7 @@ public class ShoppingBasket {
     public double getTotal() {
         double sum = 0.0;
         for (Item shoppingBasketItem : shoppingBasketItems) {
-            // this code below demonstrates a code smell called "feature envy."
-            //
-            // the Item class has envy over the features of Item methods
-            // getUnitPrice() and getQuantity() because the getTotal() method in ShoppingBasket
-            // uses the Item objects (i.e., getUnitPrice() and getQuantity() methods) inside Item
-            // MORE OFTEN than the ShoppingBasket objects.
-            // therefore, the expression shoppingBasketItem.getUnitPrice() * shoppingBasketItem.getQuantity()
-            // is in the wrong class! it belongs inside of Item instead of ShoppingBasket!
-
-            // good tip: put the work where the data is.
-            // if Item is the object responsible for calculating the subtotal, put the getSubtotal()
-            // method there instead of in ShoppingBasket.
             sum += shoppingBasketItem.getSubtotal();
-
         }
         return sum;
     }
