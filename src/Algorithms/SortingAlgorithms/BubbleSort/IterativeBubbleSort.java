f662ef3755431f6ec41637078db67af1c2e880da
DannyChen <dannychen27@hotmail.com>
2021-07-10 11:54:16 -0400

    I wrote unittests for iterative bubble sort on Day 29 of 30 of learning Java. (July 10, 2021)
    - I made the class IterativeBubbleSort and all its methods package private.
    - I removed the main() method so the iterativeBubbleSort() method doesn't have to be a static method.

--- src/Algorithms/SortingAlgorithms/BubbleSort/IterativeBubbleSort.java
+++ src/Algorithms/SortingAlgorithms/BubbleSort/IterativeBubbleSort.java
@@ -1,19 +1,11 @@
 package Algorithms.SortingAlgorithms.BubbleSort;
 
-import java.util.Arrays;
-
-public class IterativeBubbleSort {
-
-    public static void main(String[] args) {
-        int[] numbers = {5, 4, 3, 2, 1};
-        iterativeBubbleSort(numbers);
-        System.out.println(Arrays.toString(numbers)); // [1, 2, 3, 4, 5]
-    }
+class IterativeBubbleSort {
 
     /**
      * Sort this array.
      */
-    private static void iterativeBubbleSort(int[] array) {
+    void iterativeBubbleSort(int[] array) {
         // endIndex is the index of the last unsorted item.
         // loop invariant: The subarray[endIndex:array.length] is sorted.
         for (int endIndex = array.length - 1; endIndex > 0; endIndex--) {
@@ -28,7 +20,7 @@ public class IterativeBubbleSort {
     /**
      * Swap the elements at index1 and index2.
      */
-    private static void swap(int[] array, int index1, int index2) {
+    private void swap(int[] array, int index1, int index2) {
         int temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
