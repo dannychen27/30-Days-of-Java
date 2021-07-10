e9bbdd4c4a5e30188f1645aaf9897973ccf07ecf
DannyChen <dannychen27@hotmail.com>
2021-07-10 12:44:34 -0400

    I wrote unittests for in-place quick sort on Day 29 of 30 of learning Java. (July 10, 2021)
    - I made the class InPlaceQuicksort and all its methods package private.
    - I removed the main() method so all InPlaceQuicksort methods don't have to be static methods.
    
    - Unfortunately, only 6 of 7 tests pass. I should investigate what causes the last test to fail. Perhaps a StackOverflowError?

--- src/Algorithms/SortingAlgorithms/QuickSort/InPlaceQuicksort.java
+++ src/Algorithms/SortingAlgorithms/QuickSort/InPlaceQuicksort.java
@@ -1,19 +1,11 @@
 package Algorithms.SortingAlgorithms.QuickSort;
 
-import java.util.Arrays;
-
-public class InPlaceQuicksort {
-
-    public static void main(String[] args) {
-        int[] numbers = {5, 4, 3, 2, 1};
-        inPlaceQuickSort(numbers);
-        System.out.println(Arrays.toString(numbers)); // [1, 2, 3, 4, 5]
-    }
+class InPlaceQuicksort {
 
     /**
      * Sort this array.
      */
-    private static void inPlaceQuickSort(int[] array) {
+    void inPlaceQuickSort(int[] array) {
         inPlaceQuickSortHelper(array, 0, array.length - 1);
     }
 
@@ -22,7 +14,7 @@ public class InPlaceQuicksort {
      *
      * Precondition: startIndex >= 0 and endIndex >= 0.
      */
-    private static void inPlaceQuickSortHelper(int[] array, int startIndex, int endIndex) {
+    private void inPlaceQuickSortHelper(int[] array, int startIndex, int endIndex) {
         // source: https://www.google.com/search?q=quicksort+in+place+algorithm&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjIhN7guvjwAhVFGs0KHZfOBOoQ_AUoAnoECAEQBA&biw=945&bih=596#imgrc=pk3qCk0Q1TnVbM
         if (startIndex < endIndex) {
             int pivotIndex = partition(array, startIndex, endIndex);
@@ -35,7 +27,7 @@ public class InPlaceQuicksort {
      * Return the partition index after performing in place partition on the subarray
      * array[startIndex:endIndex].
      */
-    private static int partition(int[] array, int startIndex, int endIndex) {
+    private int partition(int[] array, int startIndex, int endIndex) {
         int initialPivotIndex = startIndex; // the pivot doesn't matter
         swap(array, initialPivotIndex, endIndex);
         int pivotIndex = startIndex;
@@ -52,7 +44,7 @@ public class InPlaceQuicksort {
     /**
      * Swap the elements at index1 and index2.
      */
-    private static void swap(int[] array, int index1, int index2) {
+    private void swap(int[] array, int index1, int index2) {
         int temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
