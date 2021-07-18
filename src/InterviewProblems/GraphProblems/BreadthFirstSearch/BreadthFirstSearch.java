dd5cb322993d822b6c5c4e24e43bb1cd7a8f3466
DannyChen <dannychen27@hotmail.com>
2021-07-17 22:29:15 -0400

    I wrote unittests for the breadth first search interview problem on Day 30 of 30 of learning Java. (July 17, 2021)
    - I made the class BreadthFirstSearch and all its methods package private.
    - I removed the main() method so all BreadthFirstSearch methods don't have to be static methods.

--- src/InterviewProblems/GraphProblems/BreadthFirstSearch/BreadthFirstSearch.java
+++ src/InterviewProblems/GraphProblems/BreadthFirstSearch/BreadthFirstSearch.java
@@ -13,41 +13,13 @@ import static DataStructures.CustomDataStructures.Graph.AdjacencyListImplementat
 
 class BreadthFirstSearch {
 
-    public static void main(String[] args) {
-        // BFS source code: https://mcsapps.utm.utoronto.ca/slidechat/5f49771dd89c8059833766fc/24
-        // source: https://www.youtube.com/watch?v=0XgVhsMOcQM&list=PLI1t_8YX-ApvMthLj56t1Rf-Buio5Y8KL&index=4&t=53s
+    // BFS source code: https://mcsapps.utm.utoronto.ca/slidechat/5f49771dd89c8059833766fc/24
+    // source: https://www.youtube.com/watch?v=0XgVhsMOcQM&list=PLI1t_8YX-ApvMthLj56t1Rf-Buio5Y8KL&index=4&t=53s
 
-        // time: Theta(|V| + |E|)
-        // space: Theta(|V| + |E|)
+    // time: Theta(|V| + |E|)
+    // space: Theta(|V| + |E|)
 
-        Graph graph = new Graph();
-        Vertex vertex1 = new Vertex(1);
-        Vertex vertex2 = new Vertex(2);
-        Vertex vertex3 = new Vertex(3);
-        Vertex vertex4 = new Vertex(4);
-        Vertex vertex5 = new Vertex(5);
-        Vertex vertex6 = new Vertex(6);
-        Vertex vertex7 = new Vertex(7);
-
-        graph.addVertex(vertex1);
-        graph.addVertex(vertex2);
-        graph.addVertex(vertex3);
-        graph.addVertex(vertex4);
-        graph.addVertex(vertex5);
-        graph.addVertex(vertex6);
-        graph.addVertex(vertex7);
-
-        graph.addEdge(vertex1, vertex2);
-        graph.addEdge(vertex1, vertex3);
-        graph.addEdge(vertex2, vertex4);
-        graph.addEdge(vertex2, vertex5);
-        graph.addEdge(vertex3, vertex6);
-        graph.addEdge(vertex3, vertex7);
-
-        System.out.println(breadthFirstSearch(graph, vertex1));  // [1, 2, 3, 4, 5, 6, 7]
-    }
-
-    private static List<Vertex> breadthFirstSearch(Graph graph, Vertex sourceVertex) {
+    List<Vertex> breadthFirstSearch(Graph graph, Vertex sourceVertex) {
         Map<Vertex, String> verticesToColors = new HashMap<>();
         Map<Vertex, Integer> verticesToDistances = new HashMap<>();
         for (Vertex vertex : graph.getVertices()) {
