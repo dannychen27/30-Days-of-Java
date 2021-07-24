fdfeca19bd0548160fd60a454371be836c08048e
DannyChen <dannychen27@hotmail.com>
2021-07-24 02:25:26 -0400

    I added more potential methods for the Graph interface.

--- src/DataStructures/CustomDataStructures/Graph/Graph.java
+++ src/DataStructures/CustomDataStructures/Graph/Graph.java
@@ -16,16 +16,27 @@ public interface Graph {
     // edge and vertex queries
     // boolean isAdjacent(Vertex vertex1, Vertex vertex2);
     // List<Vertex> getNeighbors(Vertex vertex);
-    // int getDegree(Vertex vertex);  // TODO; or indegree/outdegree methods for directed graphs
+
+    // TODO: for undirected graphs only
+    // int getDegree(Vertex vertex);
+
+    // TODO: for directed graphs only
+    // int getInDegree(Vertex vertex);
+    // int getOutDegree(Vertex vertex);
+
+    // TODO: for unweighted graphs, weights are either 0 or 1.
+    // TODO: for weighted graphs, weights are either real numbers or infinity.
+
     // List<Vertex> getPath(Vertex startVertex, Vertex endVertex);
     // boolean hasPath(Vertex startVertex, Vertex endVertex);  // TODO: simple vs. complex path?
     // int getPathLength(Vertex startVertex, Vertex endVertex);
-    // boolean isConnected(Vertex vertex1, Vertex vertex2);
-    // boolean isConnected();
+    // boolean isConnected(Vertex vertex1, Vertex vertex2); // vertex connectedness
+    // boolean isConnected();  // graph connectedness
     // boolean hasCycle();
     // boolean isTree();
     // boolean isSpanningTree();
     // boolean isMinimumSpanningTree();
     // boolean isSubgraph(Graph otherGraph);
+    // boolean isComplete();
 }
 
