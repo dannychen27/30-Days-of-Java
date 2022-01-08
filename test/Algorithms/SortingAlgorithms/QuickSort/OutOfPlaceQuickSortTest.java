baf002a97d6dafa7b49f22328db758837048065c
DannyChen <dannychen27@hotmail.com>
2022-01-08 03:04:57 -0500

    Update the Java 5 Assertions calls to Java 6 Assertions calls for all of the sorting algorithms.

--- test/Algorithms/SortingAlgorithms/QuickSort/OutOfPlaceQuickSortTest.java
+++ test/Algorithms/SortingAlgorithms/QuickSort/OutOfPlaceQuickSortTest.java
@@ -1,22 +1,21 @@
 package Algorithms.SortingAlgorithms.QuickSort;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class OutOfPlaceQuickSortTest {
 
     private OutOfPlaceQuickSort quickSort;
 
-    private int[] emptyArray = new int[0];
-    private int[] oneElementArray = new int[] {1};
-    private int[] sortedArray = new int[] {1, 2, 3, 4, 5};
-    private int[] almostSortedArray = new int[] {1, 2, 3, 5, 4};
-    private int[] unsortedArray = new int[] {3, 2, 5, 4, 1};
-    private int[] reversedArray = new int[] {5, 4, 3, 2, 1};
-    private int[] duplicatesArray = new int[] {2, 2, 1, 1, 3, 3};
+    private final int[] emptyArray = new int[0];
+    private final int[] oneElementArray = new int[] {1};
+    private final int[] sortedArray = new int[] {1, 2, 3, 4, 5};
+    private final int[] almostSortedArray = new int[] {1, 2, 3, 5, 4};
+    private final int[] unsortedArray = new int[] {3, 2, 5, 4, 1};
+    private final int[] reversedArray = new int[] {5, 4, 3, 2, 1};
+    private final int[] duplicatesArray = new int[] {2, 2, 1, 1, 3, 3};
 
     @BeforeEach
     void setUp() {
@@ -28,6 +27,48 @@ class OutOfPlaceQuickSortTest {
 
     }
 
+    @Test
+    void testIsSortedOnEmptyArray() {
+        quickSort.outOfPlaceQuickSort(emptyArray);
+        Assertions.assertTrue(isSorted(emptyArray));
+    }
+
+    @Test
+    void testIsSortedOnOneElementArray() {
+        quickSort.outOfPlaceQuickSort(oneElementArray);
+        Assertions.assertTrue(isSorted(oneElementArray));
+    }
+
+    @Test
+    void testIsSortedOnAlreadySortedArray() {
+        quickSort.outOfPlaceQuickSort(sortedArray);
+        Assertions.assertTrue(isSorted(sortedArray));
+    }
+
+    @Test
+    void testIsSortedOnAlmostSortedArray() {
+        quickSort.outOfPlaceQuickSort(almostSortedArray);
+        Assertions.assertTrue(isSorted(almostSortedArray));
+    }
+
+    @Test
+    void testIsSortedOnUnsortedArray() {
+        quickSort.outOfPlaceQuickSort(unsortedArray);
+        Assertions.assertTrue(isSorted(unsortedArray));
+    }
+
+    @Test
+    void testIsSortedOnArrayWithDuplicates() {
+        quickSort.outOfPlaceQuickSort(duplicatesArray);
+        Assertions.assertTrue(isSorted(duplicatesArray));
+    }
+
+    @Test
+    void testIsSortedOnReversedArray() {
+        quickSort.outOfPlaceQuickSort(reversedArray);
+        Assertions.assertTrue(isSorted(reversedArray));
+    }
+
     private static boolean isSorted(int[] array) {
         for (int i = 1; i < array.length; i++) {
             if (array[i - 1] > array[i]) {
@@ -35,47 +76,5 @@ class OutOfPlaceQuickSortTest {
             }
         }
         return true;
-    }
-
-    @Test
-    void testIsSortedOnEmptyArray() {
-        quickSort.outOfPlaceQuickSort(emptyArray);
-        assertTrue(isSorted(emptyArray));
-    }
-
-    @Test
-    void testIsSortedOnOneElementArray() {
-        quickSort.outOfPlaceQuickSort(oneElementArray);
-        assertTrue(isSorted(oneElementArray));
-    }
-
-    @Test
-    void testIsSortedOnAlreadySortedArray() {
-        quickSort.outOfPlaceQuickSort(sortedArray);
-        assertTrue(isSorted(sortedArray));
-    }
-
-    @Test
-    void testIsSortedOnAlmostSortedArray() {
-        quickSort.outOfPlaceQuickSort(almostSortedArray);
-        assertTrue(isSorted(almostSortedArray));
-    }
-
-    @Test
-    void testIsSortedOnUnsortedArray() {
-        quickSort.outOfPlaceQuickSort(unsortedArray);
-        assertTrue(isSorted(unsortedArray));
-    }
-
-    @Test
-    void testIsSortedOnArrayWithDuplicates() {
-        quickSort.outOfPlaceQuickSort(duplicatesArray);
-        assertTrue(isSorted(duplicatesArray));
-    }
-
-    @Test
-    void testIsSortedOnReversedArray() {
-        quickSort.outOfPlaceQuickSort(reversedArray);
-        assertTrue(isSorted(reversedArray));
     }
 }
--- test/Algorithms/SortingAlgorithms/RadixSort/RadixSortTest.java
+++ test/Algorithms/SortingAlgorithms/RadixSort/RadixSortTest.java
@@ -1,22 +1,21 @@
 package Algorithms.SortingAlgorithms.RadixSort;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RadixSortTest {
 
     private RadixSort radixSort;
 
-    private long[] emptyArray = new long[0];
-    private long[] oneElementArray = new long[] {1};
-    private long[] sortedArray = new long[] {1, 2, 3, 4, 5};
-    private long[] almostSortedArray = new long[] {1, 2, 3, 5, 4};
-    private long[] unsortedArray = new long[] {53, 89, 1000, 150, 36, 633, 234};
-    private long[] reversedArray = new long[] {5000, 1000, 500, 100, 50, 10, 5, 1};
-    private long[] duplicatesArray = new long[] {2, 2, 1, 1, 3, 3};
+    private final long[] emptyArray = new long[0];
+    private final long[] oneElementArray = new long[] {1};
+    private final long[] sortedArray = new long[] {1, 2, 3, 4, 5};
+    private final long[] almostSortedArray = new long[] {1, 2, 3, 5, 4};
+    private final long[] unsortedArray = new long[] {53, 89, 1000, 150, 36, 633, 234};
+    private final long[] reversedArray = new long[] {5000, 1000, 500, 100, 50, 10, 5, 1};
+    private final long[] duplicatesArray = new long[] {2, 2, 1, 1, 3, 3};
 
     @BeforeEach
     void setUp() {
@@ -28,6 +27,48 @@ class RadixSortTest {
 
     }
 
+    @Test
+    void testIsSortedOnEmptyArray() {
+        radixSort.radixSort(emptyArray);
+        Assertions.assertTrue(isSorted(emptyArray));
+    }
+
+    @Test
+    void testIsSortedOnOneElementArray() {
+        radixSort.radixSort(oneElementArray);
+        Assertions.assertTrue(isSorted(oneElementArray));
+    }
+
+    @Test
+    void testIsSortedOnAlreadySortedArray() {
+        radixSort.radixSort(sortedArray);
+        Assertions.assertTrue(isSorted(sortedArray));
+    }
+
+    @Test
+    void testIsSortedOnAlmostSortedArray() {
+        radixSort.radixSort(almostSortedArray);
+        Assertions.assertTrue(isSorted(almostSortedArray));
+    }
+
+    @Test
+    void testIsSortedOnUnsortedArray() {
+        radixSort.radixSort(unsortedArray);
+        Assertions.assertTrue(isSorted(unsortedArray));
+    }
+
+    @Test
+    void testIsSortedOnArrayWithDuplicates() {
+        radixSort.radixSort(duplicatesArray);
+        Assertions.assertTrue(isSorted(duplicatesArray));
+    }
+
+    @Test
+    void testIsSortedOnReversedArray() {
+        radixSort.radixSort(reversedArray);
+        Assertions.assertTrue(isSorted(reversedArray));
+    }
+
     private static boolean isSorted(long[] array) {
         for (int i = 1; i < array.length; i++) {
             if (array[i - 1] > array[i]) {
@@ -35,47 +76,5 @@ class RadixSortTest {
             }
         }
         return true;
-    }
-
-    @Test
-    void testIsSortedOnEmptyArray() {
-        radixSort.radixSort(emptyArray);
-        assertTrue(isSorted(emptyArray));
-    }
-
-    @Test
-    void testIsSortedOnOneElementArray() {
-        radixSort.radixSort(oneElementArray);
-        assertTrue(isSorted(oneElementArray));
-    }
-
-    @Test
-    void testIsSortedOnAlreadySortedArray() {
-        radixSort.radixSort(sortedArray);
-        assertTrue(isSorted(sortedArray));
-    }
-
-    @Test
-    void testIsSortedOnAlmostSortedArray() {
-        radixSort.radixSort(almostSortedArray);
-        assertTrue(isSorted(almostSortedArray));
-    }
-
-    @Test
-    void testIsSortedOnUnsortedArray() {
-        radixSort.radixSort(unsortedArray);
-        assertTrue(isSorted(unsortedArray));
-    }
-
-    @Test
-    void testIsSortedOnArrayWithDuplicates() {
-        radixSort.radixSort(duplicatesArray);
-        assertTrue(isSorted(duplicatesArray));
-    }
-
-    @Test
-    void testIsSortedOnReversedArray() {
-        radixSort.radixSort(reversedArray);
-        assertTrue(isSorted(reversedArray));
     }
 }
