2a94f7f1393490a41af5fd16006a20499b0306a8
DannyChen <dannychen27@hotmail.com>
2022-01-22 17:15:26 -0500

    Solve the easy LeetCode "average of levels in binary tree" problem on Day 47 of 60 of learning Java. (January 21, 2022)

---
+++ src/InterviewProblems/EasyQuestions/BFS/AverageOfLevelsInBinaryTree/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,78 @@
+package InterviewProblems.EasyQuestions.BFS.AverageOfLevelsInBinaryTree;
+
+
+import java.util.*;
+
+public class AverageOfLevelsInBinaryTree {
+
+    public static void main(String[] args) {
+        AverageOfLevelsInBinaryTree averageLevels = new AverageOfLevelsInBinaryTree();
+
+        // Example 1
+        BinaryTreeNode root1 = new BinaryTreeNode(3);
+        root1.leftChild = new BinaryTreeNode(9);
+        root1.rightChild = new BinaryTreeNode(20);
+        root1.rightChild.leftChild = new BinaryTreeNode(15);
+        root1.rightChild.rightChild = new BinaryTreeNode(7);
+        // root1.levelOrderTraversal(root1);
+        // 3
+        //     9
+        //	   20
+        //		   15
+        //		   7
+        System.out.println(averageLevels.averageOfLevels(root1));  // [3.0, 14.5, 11.0]
+
+        // Example 2
+        BinaryTreeNode root2 = new BinaryTreeNode(1);
+        root2.leftChild = new BinaryTreeNode(2);
+        root2.rightChild = new BinaryTreeNode(3);
+        root2.leftChild.leftChild = new BinaryTreeNode(4);
+        root2.leftChild.rightChild = new BinaryTreeNode(5);
+        root2.rightChild.leftChild = new BinaryTreeNode(6);
+        root2.rightChild.rightChild = new BinaryTreeNode(7);
+        // root2.levelOrderTraversal(root2);
+        // 1
+        //     2
+        //	   3
+        //		   4
+        //		   5
+        //		   6
+        //		   7
+        System.out.println(averageLevels.averageOfLevels(root2));  // [1.0, 2.5, 5.5]
+    }
+
+    public List<Double> averageOfLevels(BinaryTreeNode root) {
+        List<Double> averageAtEachLevel = new ArrayList<>();
+
+        if (root == null) {
+            return averageAtEachLevel;
+        }
+
+        Queue<BinaryTreeNode> nodesToVisit = new LinkedList<>();
+        double levelSum = 0;
+        nodesToVisit.add(root);
+
+        while (!nodesToVisit.isEmpty()) {
+            levelSum = 0;
+            int numNodesAtCurrentLevel = nodesToVisit.size();
+
+            for (int i = 0; i < numNodesAtCurrentLevel; i++) {
+                BinaryTreeNode currentNode = nodesToVisit.poll();
+                assert currentNode != null;
+                levelSum += currentNode.value;
+
+                if (currentNode.leftChild != null) {
+                    nodesToVisit.add(currentNode.leftChild);
+                }
+
+                if (currentNode.rightChild != null) {
+                    nodesToVisit.add(currentNode.rightChild);
+                }
+            }
+
+            double levelAverage = levelSum / numNodesAtCurrentLevel;
+            averageAtEachLevel.add(levelAverage);
+        }
+        return averageAtEachLevel;
+    }
+}
---
+++ src/InterviewProblems/EasyQuestions/BFS/AverageOfLevelsInBinaryTree/BinaryTreeNode.java
@@ -0,0 +1,43 @@
+package InterviewProblems.EasyQuestions.BFS.AverageOfLevelsInBinaryTree;
+
+public class BinaryTreeNode {
+
+    int value;
+    BinaryTreeNode leftChild;
+    BinaryTreeNode rightChild;
+
+    public BinaryTreeNode(int newValue) {
+        this.value = newValue;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+
+    public BinaryTreeNode(int newValue, BinaryTreeNode leftChild, BinaryTreeNode rightChild) {
+        this.value = newValue;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+
+    public void levelOrderTraversal(BinaryTreeNode root) {
+        levelOrderTraversal(root, 0);
+    }
+
+    private void levelOrderTraversal(BinaryTreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+
+        String spaces = getIndentation(depth);
+        System.out.println(spaces + root.value);
+        levelOrderTraversal(root.leftChild, depth + 1);
+        levelOrderTraversal(root.rightChild, depth + 1);
+    }
+
+    private String getIndentation(int depth) {
+        String spaces = "";
+        for (int i = 0; i < depth; i++) {
+            spaces += "\t";
+        }
+        return spaces;
+    }
+}
