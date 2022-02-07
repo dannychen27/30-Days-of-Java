0e09f204d542d9d0b41dedbb70ecb100d231e8a7
DannyChen <dannychen27@hotmail.com>
2022-02-07 03:11:20 -0500

    Add the word "unique" to the CountMazePathsTest.java file.

--- test/InterviewProblems/DynamicProgramming/CountUniqueMazePaths/CountUniqueMazePathsTest.java
+++ test/InterviewProblems/DynamicProgramming/CountUniqueMazePaths/CountUniqueMazePathsTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class CountMazePathsTest {
+class CountUniqueMazePathsTest {
 
     private CountUniqueMazePaths countMazePaths;
 
