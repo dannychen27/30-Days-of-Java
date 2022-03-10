ad86a92899617654e019562433cf0073565e1c7d
DannyChen <dannychen27@hotmail.com>
2022-03-10 15:54:48 -0500

    Solve the easy "sentence pangram" LeetCode problem on Day 66 of 120 of learning Java. (March 4, 2022)
    - I solve this problem 2 ways:
    --> with character array + character array lookup
    --> iterate through 26 character to see if each character is used

---
+++ src/InterviewProblems/Strings/CheckIfSentenceIsPangram/SentenceIsPangram.java
@@ -0,0 +1,68 @@
+package InterviewProblems.Strings.CheckIfSentenceIsPangram;
+
+public class SentenceIsPangram {
+
+    // Let n = the length of the sentence string.
+    //
+    // Time: O(n)
+    // --> We iterate through the sentence string once.
+    // Space: O(1)
+    // --> We only create an array for the 26 letters of the English alphabet.
+
+    public static void main(String[] args) {
+        SentenceIsPangram sentenceIsPangram = new SentenceIsPangram();
+
+        // Input: sentence = "leetcode"
+        // Output: false
+        // Explanation: sentence doesn't contain all the letters of the alphabet.
+        String sentence1 = "leetcode";
+        boolean isPangram1 = sentenceIsPangram.checkIfPangram(sentence1);
+        System.out.println(isPangram1);  // false
+
+        // Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+        // Output: true
+        // Explanation: sentence contains at least one of every letter of the English alphabet.
+        String sentence2 = "thequickbrownfoxjumpsoverthelazydog";
+        boolean isPangram2 = sentenceIsPangram.checkIfPangram(sentence2);
+        System.out.println(isPangram2);  // true
+
+        // Input: sentence = "thequickbrownfoxjumpsoverthelazydoo"
+        // Output: false
+        // Explanation: sentence doesn't contain all the letters of the English alphabet.
+        String sentence3 = "thequickbrownfoxjumpsoverthelazydoo";
+        boolean isPangram3 = sentenceIsPangram.checkIfPangram(sentence3);
+        System.out.println(isPangram3);  // false
+    }
+
+    /**
+     * A pangram is a sentence where every letter of the English alphabet appears
+     * at least once.
+     *
+     * Given a string sentence containing only lowercase English letters,
+     * return true iff sentence is a pangram.
+     *
+     * Preconditions:
+     * - sentence.length >= 1.
+     * - sentence consists of lowercase English letters.
+     */
+    public boolean checkIfPangram(String sentence) {
+        if (sentence.length() < 26) {
+            // A sentence with less than 26 characters cannot possibly contain
+            // all 26 letters of the English alphabet.
+            return false;
+        }
+
+        sentence = sentence.toLowerCase();
+        int[] alphabetFrequencies = new int[26];
+        for (int i = 0; i < sentence.length(); i++) {
+            alphabetFrequencies[sentence.charAt(i) - 'a']++;
+        }
+
+        for (int j : alphabetFrequencies) {
+            if (j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
