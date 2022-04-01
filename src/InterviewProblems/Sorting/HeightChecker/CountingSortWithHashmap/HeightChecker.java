63ee7a7f39be8b9863c82cd46e8ada4d7c603d7e
DannyChen <dannychen27@hotmail.com>
2022-04-01 14:10:20 -0400

    Solve the easy "height checker" LeetCode problem on Day 76 of 120 of learning Java. (April 1, 2022)
    - I solve this problem 3 ways:
    --> using a comparison-based sorting algorithm
    --> using counting sort + arrays
    --> using counting sort + hashmaps

---
+++ src/InterviewProblems/Sorting/HeightChecker/CountingSortWithHashmap/HeightChecker.java
@@ -0,0 +1,94 @@
+package InterviewProblems.Sorting.HeightChecker.CountingSortWithHashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HeightChecker {
+
+    // Let n = the length of the heights array.
+    //
+    // Time: O(n)
+    // --> O(n) for counting sort
+    // --> O(n) for cross-examining the heights and sorted height arrays.
+    // Space: O(n)
+    // --> O(n) for using counting sort to produce a newly sorted array.
+
+    public static void main(String[] args) {
+        HeightChecker heightChecker = new HeightChecker();
+
+        // Input: heights = [1, 2, 3, 4, 5]
+        // Output: 0
+        // Explanation:
+        // heights:  [1, 2, 3, 4, 5]
+        // expected: [1, 2, 3, 4, 5]
+        // All indices match.
+        int[] heights1 = {1, 2, 3, 4, 5};
+        int numIncorrectHeights1 = heightChecker.heightChecker(heights1);
+        System.out.println(numIncorrectHeights1);  // 0
+
+        // Input: heights = [1, 1, 4, 2, 1, 3]
+        // Output: 3
+        // Explanation:
+        // heights:  [1, 1, 4, 2, 1, 3]
+        // expected: [1, 1, 1, 2, 3, 4]
+        // Indices 2, 4, and 5 do not match.
+        int[] heights2 = {1, 1, 4, 2, 1, 3};
+        int numIncorrectHeights2 = heightChecker.heightChecker(heights2);
+        System.out.println(numIncorrectHeights2);  // 3
+
+        // Input: heights = [5, 1, 2, 3, 4]
+        // Output: 5
+        // Explanation:
+        // heights:  [5, 1, 2, 3, 4]
+        // expected: [1, 2, 3, 4, 5]
+        // All indices do not match.
+        int[] heights3 = {5, 1, 2, 3, 4};
+        int numIncorrectHeights3 = heightChecker.heightChecker(heights3);
+        System.out.println(numIncorrectHeights3);  // 5
+    }
+
+    /**
+     * A school is trying to take an annual photo of all the students.
+     * The students are asked to stand in a single file line in non-decreasing order
+     * by height. Let this ordering be represented by the integer array expected where
+     * expected[i] is the expected height of the ith student in line.
+     *
+     * You are given an integer array heights representing the current order that
+     * the students are standing in. Each heights[i] is the height of the ith student
+     * in line (0-indexed).
+     *
+     * Return the number of indices where heights[i] != expected[i].
+     *
+     * Preconditions:
+     * - 1 <= heights.length <= 100.
+     * - 1 <= heights[i] <= 100.
+     */
+    public int heightChecker(int[] heights) {
+        int[] sortedHeights = countingSort(heights);
+        int numIncorrectHeights = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != sortedHeights[i]) {
+                numIncorrectHeights++;
+            }
+        }
+        return numIncorrectHeights;
+    }
+
+    private int[] countingSort(int[] array) {
+        Map<Integer, Integer> numsToFrequencies = new HashMap<>();
+        for (int num : array) {
+            numsToFrequencies.put(num, numsToFrequencies.getOrDefault(num, 0) + 1);
+        }
+
+        int[] sortedArray = new int[array.length];
+        int index = 0;
+        for (int num : numsToFrequencies.keySet()) {
+            while (numsToFrequencies.get(num) > 0) {
+                sortedArray[index] = num;
+                numsToFrequencies.put(num, numsToFrequencies.get(num) - 1);
+                index++;
+            }
+        }
+        return sortedArray;
+    }
+}
