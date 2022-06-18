51835b59e268ef6c8a31a023dea5d50a6c986489
DannyChen <dannychen27@hotmail.com>
2022-06-18 15:50:40 -0400

    Solve the easy "thousand separator" LeetCode problem on Day 95 of 120 of learning Java. (June 18, 2022)
    - I solve this problem 2 ways:
    --> with stringbuilders, prepend only.
    --> with stringbuilders, append but reverse string right before I return from the thousandSeparator function.

---
+++ src/InterviewProblems/Strings/ThousandSeparator/StringBuilderAppend/ThousandSeparator.java
@@ -0,0 +1,79 @@
+package InterviewProblems.Strings.ThousandSeparator.StringBuilderAppend;
+
+public class ThousandSeparator {
+
+    // Let n = the number of digits of n.
+    //
+    // Time: O(n)
+    // --> There are n while loop iterations since we eliminate the last digit of n each iteration.
+    // --> The while loop body runs in constant time because we're appending characters to a string
+    // using a string builder.
+    // --> Right before we return from the thousandSeparator method, it takes O(n) time to
+    // reverse the entire thousand-separated digit string.
+    // Space: O(1)
+    // --> For this problem, integers can only span the closed interval [0, 2 ^ 31 - 1], so the
+    // thousand-separated digit string can only contain at most 10 digits, since 2 ^ 31 - 1 = 2,147,483,647.
+
+    public static void main(String[] args) {
+        ThousandSeparator thousandSeparator = new ThousandSeparator();
+
+        // Input: n = 0
+        // Output: "0"
+        String thousandSeparatedDigits0 = thousandSeparator.thousandSeparator(0);
+        System.out.println(thousandSeparatedDigits0);  // 0
+
+        // Input: n = 1
+        // Output: "1"
+        String thousandSeparatedDigits00 = thousandSeparator.thousandSeparator(1);
+        System.out.println(thousandSeparatedDigits00);  // 1
+
+        // Input: n = 10
+        // Output: "10"
+        String thousandSeparatedDigits000 = thousandSeparator.thousandSeparator(10);
+        System.out.println(thousandSeparatedDigits000);  // 10
+
+        // Input: n = 987
+        // Output: "987"
+        String thousandSeparatedDigits1 = thousandSeparator.thousandSeparator(987);
+        System.out.println(thousandSeparatedDigits1);  // 987
+
+        // Input: n = 1234
+        // Output: "1.234"
+        String thousandSeparatedDigits2 = thousandSeparator.thousandSeparator(1234);
+        System.out.println(thousandSeparatedDigits2);  // 1.234
+
+        // Input: n = 123456789
+        // Output: 123.456.789
+        String thousandSeparatedDigits3 = thousandSeparator.thousandSeparator(123456789);
+        System.out.println(thousandSeparatedDigits3);  // 123.456.789
+    }
+
+    /**
+     * Given an integer n, add a dot (".") as the thousands separator and return it in string format.
+     *
+     * Constraints:
+     * - 0 <= n <= 2 ^ 31 - 1.
+     */
+    public String thousandSeparator(int n) {
+        if (n == 0) {
+            return "0";
+        }
+
+        StringBuilder thousandSeparatedNumber = new StringBuilder();
+        int numDigitsSeen = 0;
+        while (n >= 1) {
+            int currentDigit = n % 10;
+            thousandSeparatedNumber.append(currentDigit);
+
+            // Add a "." after every third digit seen except for the last group
+            // of 3 digits, starting from the right of the number n.
+            if ((numDigitsSeen + 1) % 3 == 0 && n >= 10) {
+                thousandSeparatedNumber.append(".");
+            }
+
+            numDigitsSeen++;
+            n /= 10;
+        }
+        return thousandSeparatedNumber.reverse().toString();
+    }
+}
